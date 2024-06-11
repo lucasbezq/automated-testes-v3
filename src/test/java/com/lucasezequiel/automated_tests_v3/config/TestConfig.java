7c937bcbb313fc124ca352b14662a0ec28eb2103
lucasbezq <lucasbarrosez@gmail.com>
2024-06-11 01:01:18 -0300

    feat: adicionar configuração do testcontainer

---
+++ src/test/java/com/lucasezequiel/automated_tests_v3/config/TestConfig.java
@@ -0,0 +1,7 @@
+package com.lucasezequiel.automated_tests_v3.config;
+
+public class TestConfig {
+
+    public static final int SERVER_PORT = 8888;
+    public static final String CONTENT_TYPE_JSON = "application/json";
+}
